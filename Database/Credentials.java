57f803cc5209a1400e4c3476aaf31e26ff6c925c
Mii <32428593+MiiJack@users.noreply.github.com>
2023-07-11 14:45:48 +0300

    Initializing the repo

---
+++ Database/Credentials.java
@@ -0,0 +1,8 @@
+package Database;
+
+public class Credentials {
+    public static final String URL = "jdbc:postgresql://localhost:5432/tododb";
+    public static final String USER = "postgres";
+    public static final String PASSWORD= "[your-password-here]";
+}
+
